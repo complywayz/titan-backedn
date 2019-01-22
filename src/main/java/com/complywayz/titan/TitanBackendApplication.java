ffba8e68f6598b0e953682465eeca93ad343e0a3
aranga-au <aranga.nanayakkara@gmail.com>
2019-01-22 21:13:43 +1100

    package change

--- src/main/java/com/complywayz/titan/TitanBackendApplication.java
+++ src/main/java/com/complywayz/titan/TitanBackendApplication.java
@@ -1,4 +1,4 @@
-package com.complywayz.titan.titanbackend;
+package com.complywayz.titan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/complywayz/titan/TitanBackendApplicationTests.java
+++ src/test/java/com/complywayz/titan/TitanBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.complywayz.titan.titanbackend;
+package com.complywayz.titan;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
